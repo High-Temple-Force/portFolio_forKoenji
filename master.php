521f0c924cdaf25c4ef09de2b0e2b20cc7f4c63b
AokiTakuto <aokintonto14667@gmail.com>
2018-11-03 23:43:36 +0900

    select test

--- master.php
+++ master.php
@@ -5,13 +5,38 @@ if (!isset($_SESSION["NAME"])) {
     header("Location: logout.php");
     exit;
 }
+$title = Array();
+$text = Array();
+$url = Array();
+       try {
+        $pdo = new PDO ( 'mysql:dbname=koenji; host=localhost;port=3306; charset=utf8', 'root', 'Zaq12wsx!' );
+        $cmd = 'SELECT p_title, p_text, p_url FROM t_a_product;';
+        foreach($pdo->query($cmd) as $row){
+            $title[] = $row[0];
+            $text[] = $row[1];
+            $url[] = $row[2];
+        }
+    }   catch (PDOException $e) {
+        $errorMessage = 'データベースエラー';
+    }
 ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <html>
-    <h1>作成中</h1>
-        <ul>
-            <li><a href="logout.php">ログアウト</a></li>
-        </ul>
+    <head>
+
+    </head>
+
+
+    <body>
+        <?php
+            for ($i=0; $i<count($title); $i++){
+                echo $title[i];
+                echo $text[i];
+                echo $row[i];
+            }
+        ?>
+
     </body>
+
 </html>
