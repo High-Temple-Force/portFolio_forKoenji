12f3c36fb7867f3f7e8a587b28772bd0bfb878a1
AokiTakuto <aokintonto14667@gmail.com>
2018-11-03 22:20:10 +0900

    add login.php

---
+++ login.php
@@ -0,0 +1,63 @@
+<?php
+// セッション開始
+// エラーメッセージの初期化
+$errorMessage = "";
+
+// ログインボタンが押された場合
+if (isset($_POST["login"])) {
+    // 1. ユーザIDの入力チェック
+    if (empty($_POST["userid"])) {  // emptyは値が空のとき
+        $errorMessage = 'ユーザIDが未入力です。';
+    } else if (empty($_POST["password"])) {
+        $errorMessage = 'パスワードが未入力です。';
+    }
+
+    if (!empty($_POST["userid"]) && !empty($_POST["password"])) {
+        $userid = $_POST["userid"];
+        $password = $_POST["password"];
+
+        try {
+            $pdo = new PDO ( 'mysql:dbname=koenji; host=localhost;port=3306; charset=utf8', 'root', 'Zaq12wsx!' );
+            $cmd = 'SELECT * FROM t_master;';
+            foreach($pdo->query($cmd) as $row){
+                $dbuserid = $row['id'];
+                $dbpassword = $row['password'];
+            }
+            if ($password==$dbpasswd) {
+                session_regenerate_id(true);
+                $_SESSION["NAME"] = $userid;
+                header("Location: index.php");  // メイン画面へ遷移
+                exit();  // 処理終了
+            } else {
+                // 認証失敗
+                $errorMessage = 'ユーザIDあるいはパスワードに誤りがあります。';
+            }
+        } catch (PDOException $e) {
+            $errorMessage = 'データベースエラー';
+        }
+    }
+}
+?>
+
+<!doctype html>
+<html>
+    <head>
+            <meta charset="UTF-8">
+            <title>ログイン</title>
+    </head>
+    <body>
+        <h1>ログイン画面</h1>
+        <form id="loginForm" name="loginForm" action="" method="POST">
+            <fieldset>
+                <legend>ログインフォーム</legend>
+                <div><font color="#ff0000"><?php echo htmlspecialchars($errorMessage, ENT_QUOTES); ?></font></div>
+                <label for="userid">ユーザーID</label><input type="text" id="userid" name="userid" placeholder="ユーザーIDを入力" value="<?php if (!empty($_POST["userid"])) {echo htmlspecialchars($_POST["userid"], ENT_QUOTES);} ?>">
+                <br>
+                <label for="password">パスワード</label><input type="password" id="password" name="password" value="" placeholder="パスワードを入力">
+                <br>
+                <input type="submit" id="login" name="login" value="ログイン">
+            </fieldset>
+        </form>
+        <br>
+    </body>
+</html>
