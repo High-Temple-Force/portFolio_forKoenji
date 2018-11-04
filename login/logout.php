6a621335d5b63b29ec9c9abd449e4f747edca1cd
AokiTakuto <aokintonto14667@gmail.com>
2018-11-04 13:59:17 +0900

    change logout.php

--- login/logout.php
+++ login/logout.php
@@ -12,19 +12,7 @@ $_SESSION = array();
 
 // セッションクリア
 @session_destroy();
+header("Location: ../index.php");
 ?>
 
-<!doctype html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>ログアウト</title>
-    </head>
-    <body>
-        <h1>ログアウト画面</h1>
-        <div><?php echo htmlspecialchars($errorMessage, ENT_QUOTES); ?></div>
-        <ul>
-            <li><a href="login.php">ログイン画面に戻る</a></li>
-        </ul>
-    </body>
-</html>
+
