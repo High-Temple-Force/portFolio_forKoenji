5d37c23d629080288a01b177c78529b1d7c3e7b1
AokiTakuto <aokintonto14667@gmail.com>
2018-11-04 10:25:17 +0900

    main.php gradeup

--- master.php
+++ master.php
@@ -5,38 +5,86 @@ if (!isset($_SESSION["NAME"])) {
     header("Location: logout.php");
     exit;
 }
-$title = Array();
-$text = Array();
-$url = Array();
-       try {
-        $pdo = new PDO ( 'mysql:dbname=koenji; host=localhost;port=3306; charset=utf8', 'root', 'Zaq12wsx!' );
-        $cmd = 'SELECT p_title, p_text, p_url FROM t_a_product;';
-        foreach($pdo->query($cmd) as $row){
-            $title[] = $row['p_title'];
-            $text[] = $row['p_text'];
-            $url[] = $row['p_url'];
-        }
-    }   catch (PDOException $e) {
-        $errorMessage = 'データベースエラー';
+
+if (isset($_POST["submit"])) {
+    if (empty($_POST["name"])) {  // emptyは値が空のとき
+        $errorMessage = 'タイトルが未入力です。';
+    } else if (empty($_POST["text"])) {
+        $errorMessage = '説明文が未入力です。';
+    } else if (empty($_POST["link"])) {
+        $errorMessage = 'リンクが未入力です。';
     }
+    if (!empty($_POST["name"]) && !empty($_POST["text"]) && !empty($_POST["link"])) {
+        $title = $_POST["name"];
+        $text = $_POST["text"];
+        $link = $_POST["link"];
+        try {
+            $pdo = new PDO ( 'mysql:dbname=koenji; host=localhost;port=3306; charset=utf8', 'root', 'Zaq12wsx!' );
+            $cmd = 'INSERT INTO koenji.t_a_product (member_id,member_name,p_title,p_text,p_url) values (' .$title .',' .$text .',' .$link .');';
+            $pdo->query($cmd);
+        } catch (PDOException $e) {
+            $errorMessage = 'データベースエラー';
+        }
+    }
+}
 ?>
 
 <!DOCTYPE html>
 <html>
     <head>
-
+        <meta charset="utf-8">
+        <title>Portfolio form</title>
+        <link rel="stylesheet" href="form.css">
     </head>
 
 
     <body>
-        <?php
-            for ($i=0; $i<count($title); $i++){
-                echo $title[$i];
-                echo $text[$i];
-                echo $url[$i];
-            }
-        ?>
+        <div class="title">
+            <h2>
+                    Portfolio 入力Form　<br />
+            </h2>
+        </div>
+        
+        <div class="form">
+            <!--ここに、アクションのタイプ記入-->
+            <form action="" method="POST">
+                <div class="name">
+                    <h3><br />Product Name : </h3>
+                    <p>
+                        プロダクトのタイトルを入力してください。<br />  
+                            <textarea name="name" rows="1" cols="55" ></textarea><br /><br />
+                    </p>
+                </div>
 
+                <div class="text">
+                    <h3>Description : </h3>
+                     <p>
+                        プロダクトの説明文を入力してください。<br />
+                         <textarea name="text" rows="6" cols="55"></textarea><br /><br />
+        
+                    </p>
+                </div>
+
+                <div class="link">
+                    <h3>Link : </h3>
+
+                    <p>
+                        プロダクトのURLを入力してください。 <br />
+                        <textarea name="link" rows="1" cols="55"></textarea><br /><br />
+                    </p>
+                </div>
+                
+                <div class="submit">
+                    <p>
+                        <input type="submit" name="submit" value="送信">
+                    </p>
+                </div>  
+            </form>    
+
+        </div>
+        
     </body>
+
+
 
 </html>
