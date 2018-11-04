abac2955328aa3b3904f81e4a1e79ce09fcd072f
AokiTakuto <aokintonto14667@gmail.com>
2018-11-04 14:39:21 +0900

    separate user

--- login/master.php
+++ login/master.php
@@ -9,17 +9,17 @@ $Message = "";
 $name = $_SESSION["NAME"];
 $product = Array();
 $pdo = new PDO ( 'mysql:dbname=koenji; host=localhost;port=3306; charset=utf8', 'root', 'Zaq12wsx!' );
-if ($name == "aoki") {  
+if ($name == "takuto") {  
     $cmd = 'SELECT p_title,p_text,p_url from t_a_product;';
     foreach($pdo->query($cmd) as $row){
         $product[] = $row;
     }
-} elseif ($name == "mori") {
+} elseif ($name == "hayato") {
     $cmd = 'SELECT p_title,p_text,p_url from t_m_product;';
     foreach($pdo->query($cmd) as $row){
         $product[] = $row;
     }
-} elseif ($name == "yokoi") {
+} elseif ($name == "daiki") {
     $cmd = 'SELECT p_title,p_text,p_url from t_y_product;';
     foreach($pdo->query($cmd) as $row){
         $product[] = $row;
