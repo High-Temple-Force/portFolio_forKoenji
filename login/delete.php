71e11c0f4fad8d22e41d1430e48f923535f5fd5c
bluesDD <dd11blues@gmail.com>
2018-11-07 10:08:52 +0900

    btn処理を追加

--- login/delete.php
+++ login/delete.php
@@ -6,23 +6,26 @@ $errorMessage = "";
 
 
 // 削除ボタンが押された場合
-if (isset($_POST["delete"])) {
+if (isset($_POST["btn_delete"])) {
     $Message = "";
     $name = $_SESSION["NAME"];
     $product = Array();
     $pdo = new PDO ( 'mysql:dbname=koenji; host=localhost;port=3306; charset=utf8', 'root', 'Zaq12wsx!' );
+    //takuto
     if ($name == "takuto") { 
-        $cmd = 'SELECT p_title,p_text,p_url from t_a_product;';
+        $cmd = 'DELETE p_title,p_text,p_url from t_a_product;';
         foreach($pdo->query($cmd) as $row){
             $product[] = $row;
         }
+    //hayato
     } elseif ($name == "hayato") {
-        $cmd = 'SELECT p_title,p_text,p_url from t_m_product;';
+        $cmd = 'DELETE p_title,p_text,p_url from t_m_product;';
         foreach($pdo->query($cmd) as $row){
             $product[] = $row;
         }
+    //daiki
     } elseif ($name == "daiki") {
-        $cmd = 'SELECT p_title,p_text,p_url from t_y_product;';
+        $cmd = 'DELETE p_title,p_text,p_url from t_y_product;';
         foreach($pdo->query($cmd) as $row){
             $product[] = $row;
         }
--- login/master.php
+++ login/master.php
@@ -195,6 +195,8 @@ if ( !empty($_POST["confirm"])) {
                                 print '<h5 class="his-content">' .$p[0] .'<br>';
                                 print '<p class="content-text">' .$p[1] .'</p>';
                                 print '<a href="' .$p[2] .'" class="his-link">link</a>';
+                                print '<input type="submit" name="btn_edit" value="編集">';
+                                print '<input type="submit" name="btn_delete" value="削除">';
                                 print '</h5>';
                                 print '</div>';
                             }
