58dae8108ee8bc1660beea78199aa8794732f46e
bluesDD <dd11blues@gmail.com>
2018-11-09 11:35:28 +0900

    made it simple10

--- login/master.php
+++ login/master.php
@@ -211,6 +211,10 @@ function del_btn($arrayvalue) {
 
         <?php if ($page_flag === 1): ?>
             <div class="tabs">
+                <input id="add" type="radio" name="tab_item" checked>
+                <label class="tab_item" for="add">新規追加</label>
+                <input id="change" type="radio" name="tab_item" >
+                <label class="tab_item" for="change">内容変更</label>
                 <div class="tab_content" id="add_content">
                     <div class="tab_content_description">
                         <form action="" method="POST">
