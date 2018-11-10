f3f32ee9971093f10ed10b81720223e05887a441
AokiTakuto <aokintonto14667@gmail.com>
2018-11-10 11:49:14 +0900

    add delete function

--- login/master.php
+++ login/master.php
@@ -174,10 +174,20 @@ if (isset($_POST["btn_delete"])) {
                                 print '</div>';
                             }
                         ?>
+                        <script>
+                            $('form').submit(function(){
+                                var scroll_top = $(window).scrollTop();  //送信時の位置情報を取得
+                                $('input.st',this).prop('value',scroll_top);  //隠しフィールドに位置情報を設定
+                            });
+                            
+                            window.onload = function(){
+                                //ロード時に隠しフィールドから取得した値で位置をスクロール
+                                $(window).scrollTop(<?php echo @$_REQUEST['scroll_top']; ?>);
+                            }
+                        </script>
                     </div>
                 </div>
             </div>
         </div>
-        <script type="text/javascript" src="js/script.js" charset="utf-8"></script>
     </body>
 </html>
